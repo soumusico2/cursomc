b7a4767dd9255d6ab9cb64359bc7067dd7e30462
Hebert Nunes <hebertcunha1@gmail.com>
2021-02-15 11:39:29 -0300

    cliente, tipoCliente, telefones, enderecos

---
+++ src/main/java/com/hebertnunes/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.hebertnunes.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hebertnunes.cursomc.domain.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+}
---
+++ src/main/java/com/hebertnunes/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.hebertnunes.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hebertnunes.cursomc.domain.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
+
+}
