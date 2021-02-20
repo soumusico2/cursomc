3fda051772f5d07ea2409bcbaa3e3df470a47eb9
Hebert Nunes <hebertcunha1@gmail.com>
2021-02-20 19:36:23 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/hebertnunes/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/hebertnunes/cursomc/resource/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.hebertnunes.cursomc.resource;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.hebertnunes.cursomc.domain.Categoria;
+import com.hebertnunes.cursomc.dto.CategoriaDTO;
 import com.hebertnunes.cursomc.services.CategoriaService;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -65,5 +68,15 @@ public class CategoriaResource {
 		categoriaService.delete(id);
 		
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll(){
+		
+		List<Categoria> list = categoriaService.findAll();
+		
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDto);
 	}
 }
