cac0dac715bf2e2875f51004cedc77460b772aaf
Hebert Nunes <hebertcunha1@gmail.com>
2021-02-21 11:06:59 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/hebertnunes/cursomc/services/CategoriaService.java
+++ src/main/java/com/hebertnunes/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import com.hebertnunes.cursomc.domain.Categoria;
 import com.hebertnunes.cursomc.repositories.CategoriaRepository;
@@ -54,6 +57,11 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		
 		return categoriaRepository.findAll();
+	}
 	
+	public Page<Categoria> findPage(Integer page, Integer linesPorPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPorPage, Direction.valueOf(direction), orderBy);
+		
+		return categoriaRepository.findAll(pageRequest);
 	}
 }
