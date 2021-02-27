107e3707a1c0cf6bc3b8a66efc7d849b72e40bc4
Hebert Nunes <hebertcunha1@gmail.com>
2021-02-27 17:01:34 -0300

    Ajustando e testando a delecao de cliente

--- src/main/java/com/hebertnunes/cursomc/services/ClienteService.java
+++ src/main/java/com/hebertnunes/cursomc/services/ClienteService.java
@@ -61,7 +61,7 @@ public class ClienteService {
 		try {
 			clienteRepository.deleteById(id);
 		}catch(DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir porque há enteidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionadas");
 		}
 		
 	}
