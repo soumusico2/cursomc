caec6b4226ff7aef8ed5c76421e2393346a6508a
Hebert Nunes <hebertcunha1@gmail.com>
2021-02-28 16:41:34 -0300

    Inserindo Pedido

--- src/main/java/com/hebertnunes/cursomc/resource/PedidoResource.java
+++ src/main/java/com/hebertnunes/cursomc/resource/PedidoResource.java
@@ -1,13 +1,21 @@
 package com.hebertnunes.cursomc.resource;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.hebertnunes.cursomc.domain.Categoria;
 import com.hebertnunes.cursomc.domain.Pedido;
+import com.hebertnunes.cursomc.dto.CategoriaDTO;
 import com.hebertnunes.cursomc.services.PedidoService;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -25,5 +33,20 @@ public class PedidoResource {
 		Pedido pedido = pedidoService.find(id);
 		
 		return ResponseEntity.ok().body(pedido);
+	}
+	
+	
+	
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) {
+		
+		obj = pedidoService.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+		
 	}
 }
--- src/main/java/com/hebertnunes/cursomc/services/PedidoService.java
+++ src/main/java/com/hebertnunes/cursomc/services/PedidoService.java
@@ -1,12 +1,20 @@
 package com.hebertnunes.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.hebertnunes.cursomc.domain.ItemPedido;
+import com.hebertnunes.cursomc.domain.PagamentoComBoleto;
 import com.hebertnunes.cursomc.domain.Pedido;
+import com.hebertnunes.cursomc.domain.enums.EstadoPagamento;
+import com.hebertnunes.cursomc.repositories.ItemPedidoRepository;
+import com.hebertnunes.cursomc.repositories.PagamentoRepository;
 import com.hebertnunes.cursomc.repositories.PedidoRepository;
+import com.hebertnunes.cursomc.repositories.ProdutoRepository;
 import com.hebertnunes.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,11 +23,47 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository pedidoRepository;
 	
+	@Autowired
+	BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
 	public Pedido find(Integer id) throws ObjectNotFoundException {
 		
 		Optional <Pedido> pedido = pedidoRepository.findById(id);
 		
 		return pedido.orElseThrow(() -> 
 			new ObjectNotFoundException("Objeto não encontrado! Id: " +id+ ", Tipo: " +Pedido.class.getName()));
+	}
+	
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pgto = (PagamentoComBoleto)obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pgto, obj.getInstante());
+		}
+		
+		obj = pedidoRepository.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		
+		for(ItemPedido ip : obj.getItens()) { 
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 }
